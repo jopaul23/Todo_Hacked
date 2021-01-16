0bd764923be930447b31a424044719d8222b0be6
Naighu <naighu101010@gmail.com>
2021-01-16 11:19:56 +0530

    updated clock

--- lib/Helper Widgets/Clock/Providers/time_provider.dart
+++ lib/Helper Widgets/Clock/Providers/time_provider.dart
@@ -5,6 +5,7 @@ class TimeProvider {
   String time = "12:00";
   void updateHour(String hour, bool isBeforeNoon) {
     final List split = time.split(":");
+    print(hour);
     if (isBeforeNoon) {
       if (hour.length == 1)
         hour = "0" + hour;
@@ -19,6 +20,25 @@ class TimeProvider {
   void updateHourIgnoringAmPm(String hour) {
     final List split = time.split(":");
 
+    if (hour.length == 1)
+      hour = "0" + hour;
+    else if (hour == "12") hour = "00";
+
+    split[0] = hour;
+    time = split[0] + ":" + split[1];
+  }
+
+  void changeTo24(String hour) {
+    print(hour);
+    final List split = time.split(":");
+
+    split[0] = (int.parse(hour) + 12).toString();
+    time = split[0] + ":" + split[1];
+  }
+
+  void changeTo12(String hour) {
+    final List split = time.split(":");
+    hour = (int.parse(hour) - 12).toString();
     if (hour.length == 1)
       hour = "0" + hour;
     else if (hour == "12") hour = "00";
